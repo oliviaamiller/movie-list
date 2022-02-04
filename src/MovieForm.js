41cc72ca237b0de498e10d0ce921790ce5a8a31f
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-04 09:10:37 -0800

    passed props to movieform

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function MovieForm() {
+export default function MovieForm({ submitMovie,
+  movieFormTitle,
+  setMovieFormTitle,
+  movieFormDirector,
+  setMovieFormDirector,
+  movieFormYearReleased,
+  setMovieFormYearReleased,
+  movieFormColor,
+  setMovieFormColor }) {
   return <div></div>;
 }
