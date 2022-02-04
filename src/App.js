ae4963543e7163b17674cbc95aec386e6f9ed079
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-03 17:06:52 -0800

    added submitMovie function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,39 @@ import { useState } from 'react';
 import Movie from './Movie';
 import MovieList from './MovieList';
 import MovieForm from './MovieForm';
+import userEvent from '@testing-library/user-event';
 
 function App() {
+
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [movieFormYearReleased, setMovieFormYearReleased] = useState([]);
+  const [movieFormDirector, setMovieFormDirector] = useState([]);
+  const [movieTitle, setMovieTitle] = useState([]);
+  const [movieFormColor, setMovieFormColor] = useState([]);
+
+  function submitMovie(e) {
+    e.preventDefault();
+
+    const movie = {
+      id: Math.floor(Math.random() * 1000),
+      title: movieTitle, 
+      director: movieFormDirector, 
+      year: movieFormYearReleased,
+      color: movieFormColor
+    };
+
+    setAllMovies([...allMovies, movie]);
+
+    setMovieTitle('');
+    setMovieFormDirector('');
+    setMovieFormYearReleased('');
+    setMovieFormColor('');
+
+  }
+
+
+
   return (
     <div className="App">
       <Movie />
