e5936f313119e6ceb053373e9e4b3ccaa69d193c
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-04 09:29:34 -0800

    added onchange and submit to movie form

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -9,20 +9,19 @@ export default function MovieForm({ submitMovie,
   setMovieFormYearReleased,
   movieFormColor,
   setMovieFormColor }) {
-  return <div>
-    <form className='movie-form'>
-      <input placeholder='title'/>
-      <input placeholder='director' />
-      <input placeholder='year released' />
-      <select>
-        <option>red</option>
-        <option>yellow</option>
-        <option>blue</option>
-        <option>green</option>
+
+  return <div className='movie-form-container'>
+    <form className='movie-form' onSubmit={submitMovie}>
+      <input placeholder='title' required value={movieFormTitle} onChange={(e) => setMovieFormTitle(e.target.value)} />
+      <input placeholder='director' required value={movieFormDirector} onChange={(e) => setMovieFormDirector(e.target.value)} />
+      <input placeholder='year released' required value={movieFormYearReleased} onChange={(e) => setMovieFormYearReleased(e.target.value)} />
+      <select required value={movieFormColor} onChange={(e) => setMovieFormColor(e.target.value)} >
+        <option value='red'>red</option>
+        <option value='yellow'>yellow</option>
+        <option value='blue'>blue</option>
+        <option value='green'>green</option>
       </select>
       <button>submit</button>
     </form>
-    
-
   </div>;
 }
