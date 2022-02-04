ce3b4d7de46497fbaaab2102e297c6e8eab7f50c
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-04 09:32:13 -0800

    mapped through movies array in movielist

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,5 +1,9 @@
-// import Movie from './Movie';
+import Movie from './Movie';
 
-export default function MovieList() {
-  return <div></div>;
+
+export default function MovieList({ movies }) {
+  return <div className='movie-list'>
+    {movies.map((movie, i) =>
+      <Movie key={`${movie}-${i}`} movie={movie} />)}
+  </div>;
 }
